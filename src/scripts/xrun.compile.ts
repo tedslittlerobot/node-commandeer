67fab876684b6ab680658971f0ede69db0ef3fc0
Stefan Horner <stefan.horner@umotif.com>
2024-06-07 09:46:09 +0100

    add runner and compiler runner

---
+++ src/scripts/xrun.compile.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import {existsSync, readFileSync} from 'node:fs';
+import {argv} from 'node:process';
+import {$} from 'execa';
+
+const file: Record<string, string> = JSON.parse(readFileSync('package.json', 'utf8')) as Record<string, string>;
+
+await $`npm run compile`;
+
+let entrypoint = file.name;
+
+if (!entrypoint) {
+	throw new Error('Cannot get entrypoint from package.json (property: module)');
+}
+
+entrypoint = `dist/${entrypoint}.cjs`;
+
+if (!existsSync(entrypoint)) {
+	throw new Error('No entrypoint exists at ' + entrypoint);
+}
+
+await $({stdio: 'inherit'})(entrypoint, argv.slice(2));
---
+++ src/scripts/xrun.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import {existsSync, readFileSync} from 'node:fs';
+import {argv} from 'node:process';
+import {$} from 'execa';
+
+const file: Record<string, string> = JSON.parse(readFileSync('package.json', 'utf8')) as Record<string, string>;
+
+await $`npm run build`;
+
+const entrypoint = file.module;
+
+if (!entrypoint) {
+	throw new Error('Cannot get entrypoint from package.json (property: module)');
+}
+
+if (!existsSync(entrypoint)) {
+	throw new Error('No entrypoint exists at ' + entrypoint);
+}
+
+await $({stdio: 'inherit'})('node', [entrypoint, ...argv.slice(2)]);
