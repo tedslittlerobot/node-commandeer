5cba07830549a2332013e10a2bd8ddc5f00269e1
Stefan Horner <stefan.horner@umotif.com>
2024-06-12 15:41:36 +0100

    throw errors on ultra verbose

--- src/commander/handle-errors.ts
+++ src/commander/handle-errors.ts
@@ -1,6 +1,7 @@
 import {exit, stderr} from 'node:process';
 import chalk from 'chalk';
 import type {Command} from 'commander';
+import gloucester from 'gloucester';
 import type {RegisteredCommand} from './types.js';
 
 export default function handleErrors(command: Command, config: RegisteredCommand) {
@@ -10,6 +11,10 @@ export default function handleErrors(command: Command, config: RegisteredCommand
 			await config.action(...parameters);
 		} catch (error) {
 			if (error instanceof Error) {
+				if (gloucester.is.ridiculouslyVerbose) {
+					throw error;
+				}
+
 				stderr.write(`\n${chalk.red.bold(`[${error.constructor.name}]`)} ${chalk.red(error.message)}\n`);
 				exit(1);
 			}
