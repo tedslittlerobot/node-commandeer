1f8d44c3458d13031b5122844cd4067f0675b4d7
Stefan Horner <stefan.horner@umotif.com>
2024-06-17 20:32:57 +0100

    add reversioning script

---
+++ src/scripts/reversion.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+import {existsSync, readFileSync, writeFileSync} from 'node:fs';
+import {argv, stderr} from 'node:process';
+import chalk from 'chalk';
+
+const file: Record<string, string> = JSON.parse(readFileSync('package.json', 'utf8')) as Record<string, string>;
+
+const entrypoint = file.module;
+
+if (!entrypoint) {
+	throw new Error('Cannot get entrypoint from package.json (property: module)');
+}
+
+if (!existsSync(entrypoint)) {
+	throw new Error('No entrypoint exists at ' + entrypoint);
+}
+
+const token = argv[2];
+const version = argv[3];
+
+let contents = readFileSync(entrypoint, 'utf8');
+
+stderr.write(chalk.cyan.bold(`\nRe-versioning from ${token} to ${version}\n`));
+
+contents = contents.replace(`'${token}'`, `'${version}'`);
+
+writeFileSync(entrypoint, contents, 'utf8');
+
+stderr.write(chalk.cyan.bold('Re-versioning complete\n\n'));
