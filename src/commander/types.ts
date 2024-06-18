65479490aa23a8231edf469ade33e3b78b98c29f
Stefan Horner <stefan.horner@umotif.com>
2024-06-18 17:05:50 +0100

    add options to type definition

--- src/commander/types.ts
+++ src/commander/types.ts
@@ -2,9 +2,9 @@ import {type Command} from 'commander';
 import {type GloucesterEvaluator} from 'gloucester';
 import {type Choices, type TreeValue} from 'omelette';
 
-export type CommandRegistrar = RegisteredCommand | CommandGroup;
+export type CommandRegistrar<Options = any> = RegisteredCommand<Options> | CommandGroup;
 
-export type RegisteredCommand = {
+export type RegisteredCommand<Options = any> = {
 	type: 'command';
 	name: string;
 	description?: string;
@@ -13,11 +13,11 @@ export type RegisteredCommand = {
 	completions: () => TreeValue | Choices;
 
 	action(...parameters: any[]): Promise<void>;
-	action<Options>(options: Options, command: Command): Promise<void>;
-	action<Options, A1>(a: A1, options: Options, command: Command): Promise<void>;
-	action<Options, A1, A2>(argument1: A1, argument2: A2, options: Options, command: Command): Promise<void>;
-	action<Options, A1, A2, A3>(argument1: A1, argument2: A2, argument3: A3, options: Options, command: Command): Promise<void>;
-	action<Options, A1, A2, A3, A4>(argument1: A1, argument2: A2, argument3: A3, argument4: A4, options: Options, command: Command): Promise<void>;
+	action(options: Options, command: Command): Promise<void>;
+	action<A1>(a: A1, options: Options, command: Command): Promise<void>;
+	action<A1, A2>(argument1: A1, argument2: A2, options: Options, command: Command): Promise<void>;
+	action<A1, A2, A3>(argument1: A1, argument2: A2, argument3: A3, options: Options, command: Command): Promise<void>;
+	action<A1, A2, A3, A4>(argument1: A1, argument2: A2, argument3: A3, argument4: A4, options: Options, command: Command): Promise<void>;
 };
 
 export type CommandGroup = {
